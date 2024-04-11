af35cceee80d4ff9b45af5fb0151d4d2b62d7c41
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-04-11 22:09:31 +0900

    #

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -8,33 +8,28 @@ function Header() {
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Hidden brand</a>
+                    <a class="navbar-brand" href="#!">Hidden brand</a>
                     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                         <ul class=" navbar-nav nav-tabs w-100 d-flex justify-content-end border border-danger">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Active</a>
+                                <a class="nav-link active" aria-current="page" href="#!">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="#!">About</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="#!">Services</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a class="nav-link" href="#!">Projects</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Qualifications</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Contact</a>
                             </li>
                         </ul>
-                        {/* <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul> */}
                     </div>
                 </div>
             </nav>
