2ebb1db183957a165a54af051960f0005d078d75
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-04-12 21:32:14 +0900

    #

--- src/screens/servise/Services.js
+++ src/screens/servise/Services.js
@@ -15,29 +15,29 @@ function Services() {
                     </div>
                     <div class="row row-cols-1 row-cols-md-3 g-4">
                         <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="..." />
+                            <div class="card services-card  h-100">
+                                <i class="fa-solid fa-chart-simple services-icon"></i>
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 class="card-title services-card-title">Dynamic</h5>
+                                    <p class="card-text services-card-text">Websites don't have to be static, I love making pages come to life.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="..." />
+                            <div class="card services-card h-100">
+                                <i class="fa-solid fa-tablet-screen-button services-icon"></i>
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 class="card-title services-card-title">Responsive <br /> Web Development</h5>
+                                    <p class="card-text services-card-text">Web development services include both front-end and back-end responsive development.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="..." />
+                            <div class="card services-card h-100">
+                                <i class="fa-solid fa-bolt services-icon"></i>
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                    <h5 class="card-title services-card-title">Fast</h5>
+                                    <p class="card-text services-card-text">Fast load times and lag free integrations, highest priority.</p>
                                 </div>
                             </div>
                         </div>
