5797424ca6a9caaafb914170738a06679db17d96
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-04-13 14:45:41 +0900

    #

--- src/screens/portfolio/Portfolio.js
+++ src/screens/portfolio/Portfolio.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import dw from './imgs/disinfection.jpg'
+import fs from './imgs/futureskyscrapers.jpg'
+import gr from './imgs/greenrestaruant.jpg'
+import sf from './imgs/softfurniture.jpg'
+import tg from './imgs/tillogram.jpg'
+import wn from './imgs/watchnow.jpg'
 import './Portfolio.css'
 
+const portfolioData = [
+    {
+        "id": "1",
+        "name": "Tillogram",
+        "img": tg,
+    },
+
+    {
+        "id": "2",
+        "name": "Soft Furniture",
+        "img": sf
+    },
+
+    {
+        "id": "3",
+        "name": "Future Skyscrapers",
+        "img": fs
+    },
+
+    {
+        "id": "4",
+        "name": "Disinfection",
+        "img": dw
+    },
+
+    {
+        "id": "5",
+        "name": "Green Restaruant",
+        "img": gr
+    },
+
+    {
+        "id": "6",
+        "name": "Watch Now",
+        "img": wn
+    },
+]
+
 function Portfolio() {
+
     return (
         <div class="portfolio">
             <div class="container">
@@ -23,44 +68,22 @@ function Portfolio() {
                         <li class="btn rounded-pill portfolio-types-item">Mobile Apps</li>
                     </ul>
                 </div>
-                <div class="row row-cols-1 row-cols-md-4 g-2">
-                    <div class="col">
-                        <div class="card portfolio-card  h-100">
-                            <i class="fa-solid fa-chart-simple portfolio-icon"></i>
-                            <div class="card-body">
-                                <h5 class="card-title portfolio-card-title">Dynamic</h5>
-                                <p class="card-text portfolio-card-text">Websites don't have to be static, I love making pages come to life.</p>
+                <div class="row row-cols-1 row-cols-md-3 g-2 mt-lg-4 pb-2 portfolio-cards-bg">
+                    {
+                        portfolioData.map(item => (
+                            <div class="col">
+                                <div class="card portfolio-card h-100">
+                                    <img src={item.img} alt='tg' class="portfolio-card-img" />
+                                    <div class="portfolio-card-context">
+                                        <div class="portfolio-card-context-items">
+                                            <h5 class="card-title portfolio-card-title">Dynamic</h5>
+                                            <p class="card-text portfolio-card-text">Websites don't have to be static, I love making pages come to life.</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card portfolio-card h-100">
-                            <i class="fa-solid fa-tablet-screen-button portfolio-icon"></i>
-                            <div class="card-body">
-                                <h5 class="card-title portfolio-card-title">Responsive <br /> Web Development</h5>
-                                <p class="card-text portfolio-card-text">Web development portfolio include both front-end and back-end responsive development.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card portfolio-card h-100">
-                            <i class="fa-solid fa-bolt portfolio-icon"></i>
-                            <div class="card-body">
-                                <h5 class="card-title portfolio-card-title">Fast</h5>
-                                <p class="card-text portfolio-card-text">Fast load times and lag free integrations, highest priority.</p>
-                            </div>
-                        </div><div class="col">
-                    </div>
-                    </div>
-                    <div class="col">
-                        <div class="card portfolio-card h-100">
-                            <i class="fa-solid fa-bolt portfolio-icon"></i>
-                            <div class="card-body">
-                                <h5 class="card-title portfolio-card-title">Fast</h5>
-                                <p class="card-text portfolio-card-text">Fast load times and lag free integrations, highest priority.</p>
-                            </div>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
 
